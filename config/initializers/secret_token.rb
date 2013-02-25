d1e507a29ade572f79ee6bc0ca4d4dc9666129c8
Sam Serpoosh <Sam@unknown5c969d97f1b7.att.net>
2013-02-24 23:30:12 -0500

    Blogger app. Now can add taggings for an article

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '5b3babd24b582c25ee9645c56086158e081f7e19c4310e255377dd5855be9933df00db60ddf1df9fddb1751a74dcce983f8fe45157efa6b6b000a5a60d260f36'
